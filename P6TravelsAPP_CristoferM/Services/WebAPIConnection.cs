efc8fa728074506f619902f963e374c2a69f0e3c
CRISTOFER MONTERO MONTERO <208150613@castrocarazo.ac.cr>
2024-08-02 21:35:51 -0600

    semana 12

---
+++ P6TravelsAPP_CristoferM/Services/WebAPIConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6TravelsAPP_CristoferM.Services
+{
+    public static class WebAPIConnection
+    {
+        //esta clase autoinstanciada permite configurar ruta de consumo base
+        //del servicio web API. Normalmente es DNS -> www.misitio.com/api/
+        //o con IP -> 85.25.45.10/api o local -> 192.168.100.9/api
+
+        //variable
+        public static string BaseURl = "http://192.168.100.9:45459/api/";
+
+        //incluir info de seguridad
+        //para los end point del API
+
+        public static string ApikeyName = "Apikey";
+        public static string ApikeyRadio = "CrisDP62024abx123";
+
+        //incluir forma de codificar paquetes JSON que seran enviados y recibidos del API
+
+        public static string Mimetype = "application/json";
+        public static string ContentType = "Content-Type";
+    }
+}
---
+++ P6TravelsAPP_CristoferM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace P6TravelsAPP_CristoferM.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //variable de control de uso
+        private bool isBusy = false;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        private string title = string.Empty;
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action? onChange = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value)) return false;
+
+            backingStore = value;
+            onChange?.Invoke();
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null) return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
