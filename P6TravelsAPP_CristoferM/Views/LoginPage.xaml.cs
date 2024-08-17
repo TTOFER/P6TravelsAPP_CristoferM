04546d74d24aa067c19d28b8d7df1ad6c39bf9b9
CRISTOFER MONTERO MONTERO <208150613@castrocarazo.ac.cr>
2024-08-17 01:37:45 -0600

    semana 13

--- P6TravelsAPP_CristoferM/Views/LoginPage.xaml.cs
+++ P6TravelsAPP_CristoferM/Views/LoginPage.xaml.cs
@@ -6,22 +6,4 @@ public partial class LoginPage : ContentPage
 	{
         InitializeComponent();
 	}
-
-    private void SwShowPassword_Toggled(object sender, ToggledEventArgs e)
-    {
-        TxtPassword.IsPassword = true;
-
-        if (SwShowPassword.IsToggled)
-		{
-			TxtPassword.IsPassword = false;
-		}
-
-
-
-    }
-
-    private async void BtnSignUp_Clicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new UserSignUpPage());
-    }
 }
